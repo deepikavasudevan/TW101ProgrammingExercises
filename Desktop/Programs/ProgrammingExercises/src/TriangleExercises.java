db11a8c9a5eb623591cc0ad9e7f5f0a6e26f76e2
deepika <deepikav2793@gmail.com>
2015-08-06 11:59:54 +0530

    Updating TriangleExercises.java with two more methods 3. prints n number of asterisks vertically 4. prints n lines of asterisks as a right angled triangle

--- Desktop/Programs/ProgrammingExercises/src/TriangleExercises.java
+++ Desktop/Programs/ProgrammingExercises/src/TriangleExercises.java
@@ -28,7 +28,7 @@ class TriangleExercises {
         return n;
     }
 
-    void printHorizontalAsterisx(int n) {
+    void printHorizontalAsterisxs(int n) {
         System.out.println("\n------PRINTING N ASTERISXS HORIZONTALLY------\n");
         if (n<=0){
             System.out.println("Invalid Input");
@@ -41,11 +41,46 @@ class TriangleExercises {
         System.out.println();
     }
 
+    void printVerticalAsterisks(int n) {
+        System.out.println("\n------PRINTING N ASTERISKS VERTICALLY------\n");
+
+        if (n<=0) {
+            System.out.println("Invalid Input");
+        }
+        else {
+            for (int i=1;i<=n;i++) {
+                System.out.println ("*");
+            }
+        }
+    }
+
+    void printRightAngledTriangleAsterisks(int n) {
+        System.out.println ("\n------PRINTING RIGHT ANGLED TRIANGLE OF ASTERISKS------\n");
+
+        if (n<=0) {
+            System.out.println ("Invalid Input");
+        }
+        else {
+            for (int i=1;i<=n;i++) {
+                for (int j=1;j<=i;j++) {
+                    System.out.print ("* ");
+                }
+            System.out.println();
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         TriangleExercises te1=new TriangleExercises();
         te1.printOneAsterisx();
 
         int n=te1.getNumberOfAsterisx();
-        te1.printHorizontalAsterisx(n);
+        te1.printHorizontalAsterisxs(n);
+
+        n=te1.getNumberOfAsterisx();
+        te1.printVerticalAsterisks(n);
+
+        n=te1.getNumberOfAsterisx();
+        te1.printRightAngledTriangleAsterisks(n);
     }
 }
