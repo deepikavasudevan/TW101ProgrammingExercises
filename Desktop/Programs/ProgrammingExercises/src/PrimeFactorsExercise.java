26b290ec46d8b9250ba763edc58f6d36c74d9c82
deepika <deepikav2793@gmail.com>
2015-08-07 11:28:23 +0530

    Added two methods, one that divides the prime number by 2 and the other that divides by the odd primes to get the prime factors of the number

--- Desktop/Programs/ProgrammingExercises/src/PrimeFactorsExercise.java
+++ Desktop/Programs/ProgrammingExercises/src/PrimeFactorsExercise.java
@@ -3,20 +3,37 @@
  */
 public class PrimeFactorsExercise {
 
-
-    void generate (int n){
+    int divideByTwo(int n) {
 
         while (n%2==0) {
             n=n/2;
-            System.out.println("2");
+            System.out.print("2,");
         }
+    return n;
+    }
 
+    void divideByOddPrimes(int n, int originalN) {
 
+        for (int oddNumber=3;oddNumber<=Math.sqrt(originalN); oddNumber+=2) {
 
+            while (n%oddNumber==0) {
+                n=n/oddNumber;
+                System.out.print(oddNumber+",");
+            }
+        }
+    }
+
+    void generate (int n){
+        PrimeFactorsExercise tempPfe=new PrimeFactorsExercise();
+
+        int tempN=tempPfe.divideByTwo(n);
+
+        if (tempN!=1)
+            tempPfe.divideByOddPrimes(tempN,n);
     }
 
     public static void main(String[] args) {
         PrimeFactorsExercise pfe1=new PrimeFactorsExercise();
-        pfe1.generate(30);
+        pfe1.generate(315);
     }
 }
